2596c92166b1b14213a411b3e9cca771ca3eff4a
devWeibel <weibel.jason@gmail.com>
2022-11-28 15:50:28 -0500

    added buttons to product cards

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,14 +1,31 @@
 import { Card, Button, Form, Row, Col } from 'react-bootstrap';
+import { CartContext } from "../../CartContext";
+import { useContext } from 'react';
 
 function ProductCard (props) {
     const product = props.product;
+    const cart = useContext(CartContext);
+    const productQuantity = cart.getProductQuantity(product.id);
 
     return (
         <Card>
             <Card.Body>
                 <Card.Title>{product.name}</Card.Title>
                 <Card.Text>${product.price}</Card.Text>
-                <Button variant='primary'>Add To Cart</Button>
+                { productQuantity > 0 ?
+                    <div>
+                        <Form as={Row}>
+                            <Form.Label column='true' sm='6'> In Cart: {productQuantity} </Form.Label>
+                            <Col sm='6'>
+                                <Button sm='6' onClick={ () => cart.addOne(product.id)} className='mx-2'>+</Button>
+                                <Button sm='6' onClick={ () => cart.removeOne(product.id)}className='mx-2'>-</Button>
+                            </Col>
+                        </Form>
+                        <Button variant='danger' onClick={ () => cart.deleteFromCart(product.id)} className='my-2'>Remove From Cart</Button>
+                    </div>
+                    :
+                    <Button variant='primary' onClick={ () => cart.addOne(product.id) }> Add To Cart </Button>
+                }
             </Card.Body>
         </Card>
     )
