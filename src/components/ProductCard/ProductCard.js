7da7578aeca4192d8c5950109a94663c8b40c248
devWeibel <weibel.jason@gmail.com>
2022-11-28 16:05:55 -0500

    forgot the +1 to add a product....

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -6,12 +6,14 @@ function ProductCard (props) {
     const product = props.product;
     const cart = useContext(CartContext);
     const productQuantity = cart.getProductQuantity(product.id);
+    console.log(cart.items);
 
     return (
         <Card>
             <Card.Body>
                 <Card.Title>{product.name}</Card.Title>
                 <Card.Text>${product.price}</Card.Text>
+                {/* checks product quantity to determine which buttons to show */}
                 { productQuantity > 0 ?
                     <div>
                         <Form as={Row}>
@@ -21,7 +23,7 @@ function ProductCard (props) {
                                 <Button sm='6' onClick={ () => cart.removeOne(product.id)}className='mx-2'>-</Button>
                             </Col>
                         </Form>
-                        <Button variant='danger' onClick={ () => cart.deleteFromCart(product.id)} className='my-2'>Remove From Cart</Button>
+                        <Button variant='danger' onClick={ () => cart.deleteFromCart(product.id)} className='my-2'>Remove All</Button>
                     </div>
                     :
                     <Button variant='primary' onClick={ () => cart.addOne(product.id) }> Add To Cart </Button>
