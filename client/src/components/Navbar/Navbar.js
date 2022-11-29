ff7b1e0035935f4003b98b387daca35842d4681d
devWeibel <weibel.jason@gmail.com>
2022-11-28 22:50:26 -0500

    deleted duplicate src and public folders.  migrated cart and product files

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,11 +1,16 @@
-import { Button, Container, Navbar, Modal } from 'react-bootstrap';
-import { useState } from 'react';
+import { Button, Navbar, Modal } from 'react-bootstrap';
+import { useState, useContext } from 'react';
+import { CartContext } from '../../CartContext';
 
 function NavComponent() {
-   const [ show, setShow ] = useState(false);
-   const handleShow = () => setShow(true);
-   const handleClose = () => setShow(false);
-   
+    const cart = useContext(CartContext);
+    const [show, setShow] = useState(false);
+    const handleShow = () => setShow(true);
+    const handleClose = () => setShow(false);
+
+    // adds up all product.quantity to show total amount of products in cart
+    const productCount = cart.items.reduce((sum, product) => sum + product.quantity, 0);
+    console.log(cart);
     return (
         <div>
             <Navbar expand='sm'>
@@ -14,9 +19,7 @@ function NavComponent() {
                 </Navbar.Brand>
                 <Navbar.Toggle />
                 <Navbar.Collapse className='justify-content-end'>
-                    <Button onClick={handleShow}>
-                        Cart 0 items
-                    </Button>
+                    <Button onClick={handleShow}> Cart ({ productCount }) </Button>
                 </Navbar.Collapse>
             </Navbar>
             <Modal show={show} onHide={handleClose}>
@@ -24,7 +27,22 @@ function NavComponent() {
                     <Modal.Title>Shopping Cart</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <h1>This is the modal body</h1>
+                    {/* If there are products, map over current */}
+                    { productCount > 0 ?
+                        <div>
+                            <p>In Cart: </p>
+                            { cart.items.map((currentProduct, index) => (
+                                <h2>{currentProduct.id}</h2>
+                            ))}
+                            <h2>Total: { cart.getTotalCost() }</h2>
+
+                            <Button variant='success'>Checkout</Button>
+                        </div>
+                    :
+                        <div>
+                            <p>Your Cart Is Empty!</p>
+                        </div>
+                    }
                 </Modal.Body>
             </Modal>
         </div>
--- client/src/components/ProductCard/ProductCard.js
+++ client/src/components/ProductCard/ProductCard.js
@@ -1,14 +1,33 @@
 import { Card, Button, Form, Row, Col } from 'react-bootstrap';
+import { CartContext } from "../../CartContext";
+import { useContext } from 'react';
 
 function ProductCard (props) {
     const product = props.product;
+    const cart = useContext(CartContext);
+    const productQuantity = cart.getProductQuantity(product.id);
+    // console.log(cart.items);
 
     return (
         <Card>
             <Card.Body>
                 <Card.Title>{product.name}</Card.Title>
                 <Card.Text>${product.price}</Card.Text>
-                <Button variant='primary'>Add To Cart</Button>
+                {/* checks product quantity to determine which buttons to show */}
+                { productQuantity > 0 ?
+                    <div>
+                        <Form as={Row}>
+                            <Form.Label column='true' sm='6'> In Cart: {productQuantity} </Form.Label>
+                            <Col sm='6'>
+                                <Button sm='6' onClick={ () => cart.addOne(product.id)} className='mx-2'>+</Button>
+                                <Button sm='6' onClick={ () => cart.removeOne(product.id)}className='mx-2'>-</Button>
+                            </Col>
+                        </Form>
+                        <Button variant='danger' onClick={ () => cart.deleteFromCart(product.id)} className='my-2'>Remove All</Button>
+                    </div>
+                    :
+                    <Button variant='primary' onClick={ () => cart.addOne(product.id) }> Add To Cart </Button>
+                }
             </Card.Body>
         </Card>
     )
