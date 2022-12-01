1229c568a1ce03fe046bec1b6147d685c3630cd5
Christopher Dirksen <chrisdirksen024@outlook.com>
2022-12-01 10:13:43 -0500

    commit

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,40 +1,73 @@
- import React, {useState} from 'react'
+//  import React, {useState} from 'react'
 
 
+
+// function LoginForm({Login, error}) {
+//     const [details, setDetails] = useState({email:"", password:""})
+
+//     const submitHandler = e =>{
+//         e.preventDefault();
+
+//         Login(details);
+
+//     }
+
+//   return (
+//     <form onSubmit={submitHandler}>
+//         <div>
+//             <h2>Login</h2>
+//             {/* {(error !== "") ? (<div className="error">{error}</div>) : ""} */}
+//             <div className="form-group">
+//                 <label htmlFor="email">Email:</label>
+//                 <input type="text" name="email" id="email" onChange={e => setDetails({...details, email: e.target.value})} value= {details.email} />
+//             </div>
+//             <div className="form-group">
+//                 <label htmlFor="password">Password:</label>
+//                 <input type="password" name="password" id="password" onChange={e => setDetails({...details, password: e.target.value})} value= {details.password} />
+//             </div>
+//             <div className="form-group">
+//                 <input type="submit" value="Login" />
+
+//             </div>
+//             <div>
+//                 <h4>Don't have an account?</h4>
+//                 {/* <link to="Create"><button>Click Here!</button></link> */}
+//             </div>
+//         </div>
+//     </form>
+//   )
+// }
+
+// export default LoginForm
+
+import React, {useState} from 'react'
 
 function LoginForm({Login, error}) {
-    const [details, setDetails] = useState({email:"", password:""})
-
-    const submitHandler = e =>{
+    const [details, setDetails] = useState({email:'', password:''})
+    const submitHandler =(e) => {
         e.preventDefault();
 
-        Login(details);
-
+        Login(details)
     }
 
   return (
-    <form onSubmit={submitHandler}>
-        <div>
-            <h2>Login</h2>
-            {/* {(error !== "") ? (<div className="error">{error}</div>) : ""} */}
-            <div className="form-group">
-                <label htmlFor="email">Email:</label>
-                <input type="text" name="email" id="email" onChange={e => setDetails({...details, email: e.target.value})} value= {details.email} />
-            </div>
-            <div className="form-group">
-                <label htmlFor="password">Password:</label>
-                <input type="password" name="password" id="password" onChange={e => setDetails({...details, password: e.target.value})} value= {details.password} />
-            </div>
-            <div className="form-group">
-                <input type="submit" value="Login" />
-
-            </div>
-            <div>
-                <h4>Don't have an account?</h4>
-                {/* <link to="Create"><button>Click Here!</button></link> */}
-            </div>
+    <div>
+        {(error !== "") ? (<div className="error">{error}</div>) : "" }
+        <form onSubmit={submitHandler}>
+        <h2>Login</h2>
+        <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input type="text" name="email" id="email" onChange={e => setDetails({...details, email: e.target.value})} value= {details.email} />
         </div>
-    </form>
+        <div className="form-group">
+            <label htmlFor="password">Password:</label>
+            <input type="password" name="password" id="password" onChange={e => setDetails({...details, password: e.target.value})} value= {details.password} />
+        </div>
+        <div className="form-group">
+            <input type="submit" value="Login" />
+        </div>
+        </form>
+    </div>
   )
 }
 
--- client/src/pages/UserPage.js
+++ client/src/pages/UserPage.js
@@ -1,52 +1,105 @@
-import React, { useState } from 'react';
-import LoginForm from '../components/LoginForm';
-// import Queries from '../components/Queries'
+// import React, { useState } from 'react';
+// import LoginForm from '../components/LoginForm';
+// // import Queries from '../components/Queries'
 
-const UserPage = details =>{
+// const UserPage = details =>{
 
-    const admin = {email:"admin@admin.com", password:"admin"}
-    const [user, setUser] = useState({name:"", email:""});
+//     const admin = {email:"admin@admin.com", password:"admin"}
+//     const [user, setUser] = useState({name:"", email:""});
 
-    const Login = details => {
-        console.log(details)
+//     const Login = details => {
+//         console.log(details)
         
-        if (details.email === admin.email && details.password === admin.password){
-            console.log("logged In!")
-            setUser({
-                name:details.name,
-                email:details.email
-            })}
-          }
+//         if (details.email === admin.email && details.password === admin.password){
+//             console.log("logged In!")
+
+//           }
           
-          const Logout = () => {
-              setUser({...user})
-          }
+//           const Logout = () => {
+//               setUser({...user})
+//           }
         
         
-    if (details.email===admin.email && details.password === admin.password){
-        return(
-            <div>
-                <LoginForm Login={Login} />
-            </div>
-        )
-    }else{
-        return(
-                <div>
-                    {/* <div className="welcome">
-                        <h2>Welcome, <span>{user.first_name}!</span></h2>
-                        <button onClick={Logout} >Logout</button>
-                    </div>
-                    <div>
-                        <h3>{user.first_name} {user.last_name}</h3>
-                        <p>{user.payment.card_type}  {user.payment.expiration}</p>
-                        {/* <p>**** **** **** {lastFour}</p> }
-                    </div> */}
-    <h1>IT WORKS!!!</h1>
-                </div>)
+//     if (details.email===admin.email && details.password === admin.password){
+
+//         return(
+//             <div>
+//                 {        setUser({
+//             name:details.name,
+//             email:details.email
+//         })}
+//                 <LoginForm Login={Login} />
+//             </div>
+//         )
+//     }else{
+//         return(
+//                 <div>
+//                     {/* <div className="welcome">
+//                         <h2>Welcome, <span>{user.first_name}!</span></h2>
+//                         <button onClick={Logout} >Logout</button>
+//                     </div>
+//                     <div>
+//                         <h3>{user.first_name} {user.last_name}</h3>
+//                         <p>{user.payment.card_type}  {user.payment.expiration}</p>
+//                         {/* <p>**** **** **** {lastFour}</p> }
+//                     </div> */}
+//     <h1>IT WORKS!!!</h1>
+//                 </div>)
+//     }
+// }
+// }
+
+// export default UserPage;
+
+import React, {useState} from 'react'
+import LoginForm from '../components/LoginForm'
+
+const UserPage = () => {
+
+    const admin = {
+        email:'admin@admin.com',
+        password:"admin"
     }
 
-   
+    const [user, setUser] = useState({email:"", loggedIn:false})
+    const [error, setError] = useState('')
 
+    const Login = (details) => {
+        console.log(details)
+
+        if (details.email === admin.email && details.password === admin.password){
+            console.log('LoggedIn!')
+            setUser({
+                email:details.email,
+                loggedIn:true
+            })
+        }else{
+            console.log('details dont match')
+            setError("User Not Found")
+        }
+    }
+
+    const Logout =() => {
+        console.log("logout")
+        
+        setUser({email:"", loggedIn:false})
+    }
+
+    return (
+        <div>
+            {(user.email !== "") ? (
+                <div>
+                    <h1>Welcome!</h1>
+                    <div>
+                        <button onClick={Logout} value="Logout" />
+                    </div>
+                </div>
+            ) :(
+                <LoginForm Login={Login} error={error}/>
+            )
+            }
+        </div>
+    )
 }
 
-export default UserPage;
+export default UserPage
